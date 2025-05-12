4eb113a7e685bb26f30253b311f6c95c9b9c8ee1
Fábio Santos <fabioedusantos@gmail.com>
2025-05-12 20:00:23 -0300

    test: adicionar validação de falha ao gerar código de confirmação com exceção em resendConfirmEmail no AuthServiceTest
    - adiciona testResendConfirmEmailFalhaGerarCodigoConfirmacaoException validando erro ao lançar exceção ao salvar o código de confirmação

--- app/tests/Services/AuthServiceTest.php
+++ app/tests/Services/AuthServiceTest.php
@@ -567,4 +567,34 @@ class AuthServiceTest extends TestCase
             "fake-token"
         );
     }
+
+    public function testResendConfirmEmailFalhaGerarCodigoConfirmacaoException(): void
+    {
+        $this->userData['firebase_uid'] = null;
+        $tempo = $this->expirationInHours * 60 * 60 - 1;
+
+        $recaptchaHelper = Mockery::mock('overload:' . GoogleRecaptchaHelper::class);
+        $recaptchaHelper->shouldReceive('isValid')
+            ->once()
+            ->andReturn(true);
+
+        $this->userRepository->method('getByEmailWithPasswordReset')->willReturn(
+            $this->userData +
+            [
+                'reset_code' => password_hash("123456", PASSWORD_BCRYPT),
+                'reset_code_expiry' => (new DateTime("+{$tempo} second"))->format('Y-m-d H:i:s')
+            ]
+        );
+        $this->userRepository->method('updateResetCode')->willThrowException(
+            new \PDOException("Erro no banco de dados XPTO")
+        );
+
+        $this->expectExceptionMessage("Não foi possível salvar o código de confirmação. Tente novamente.");
+
+        $this->authService->resendConfirmEmail(
+            "fabioedusantos@gmail.com",
+            "fake-token",
+            "fake-token"
+        );
+    }
 }

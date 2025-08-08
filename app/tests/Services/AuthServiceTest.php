df2949200b8d3206db3ec0f234bbfdc8aedabb2b
Fábio Santos <fabioedusantos@gmail.com>
2025-08-08 18:13:23 -0300

    test: adicionar teste testLoginGoogleFalhaAtualizarFotoUsuario em AuthServiceTest
    - adiciona testLoginGoogleFalhaAtualizarFotoUsuario que mocka GoogleRecaptcha, FirebaseAuth e repositórios de usuário; valida exceção "Erro ao atualizar foto de perfil. Tente novamente." quando atualização da foto de perfil falha no loginGoogle.

--- app/tests/Services/AuthServiceTest.php
+++ app/tests/Services/AuthServiceTest.php
@@ -2538,4 +2538,28 @@ class AuthServiceTest extends TestCase
             "fake-token",
         );
     }
+
+    public function testLoginGoogleFalhaAtualizarFotoUsuario(): void
+    {
+        $recaptchaHelper = Mockery::mock('overload:' . GoogleRecaptchaHelper::class);
+        $recaptchaHelper->shouldReceive('isValid')
+            ->once()
+            ->andReturn(true);
+
+        $firebaseAuthHelper = Mockery::mock('overload:' . FirebaseAuthHelper::class);
+        $firebaseAuthHelper->shouldReceive('verificarIdToken')
+            ->once()
+            ->andReturn($this->firebaseUserData);
+
+        $this->userRepository->method('getByFirebaseUid')->willReturn($this->userData);
+        $this->userRepository->method('updatePhotoBlob')->willReturn(false);
+
+        $this->expectExceptionMessage("Erro ao atualizar foto de perfil. Tente novamente.");
+
+        $this->authService->loginGoogle(
+            "FaKeFirebaseTokenFaKeFirebas",
+            "fake-token",
+            "fake-token",
+        );
+    }
 }
